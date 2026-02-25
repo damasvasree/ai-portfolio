f6c8e92755dc533be84d3be9d93692bd6dd58d29
Damasvasree <damasvasree@gmail.com>
2026-02-25 11:57:42 +0530

    Fix deployment cursor conflict

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,13 @@ import Skills from "./components/Skills";
 import Contact from "./components/Contact";
 
 import ParticlesBg from "./components/effects/ParticlesBg";
-import CursorGlow from "./components/CursorGlow";
-import AnimatedCursor from "react-animated-cursor";
 import GithubHeatmap from "./components/GithubHeatmap";
 import SystemFlow from "./components/SystemFlow";
 import Chatbot from "./components/Chatbot";
 import Certifications from "./components/Certifications";
 
 export default function App() {
+  
   const [dark, setDark] = useState(true);
 
   // 🌙 Dark / Light toggle logic
@@ -40,7 +39,7 @@ text-gray-900 dark:text-white
 
       {/* Effects */}
       <ParticlesBg />
-      <CursorGlow />
+    
 
       {/* Navbar (IMPORTANT — pass props) */}
       <Navbar dark={dark} setDark={setDark} />
@@ -57,15 +56,7 @@ text-gray-900 dark:text-white
       <GithubHeatmap />
       <Chatbot />
       
-      {/* Cursor */}
-      <AnimatedCursor
-        innerSize={8}
-        outerSize={35}
-        color="0,255,255"
-        outerAlpha={0.2}
-        innerScale={1}
-        outerScale={2}
-      />
+      
 
     </div>
   );
